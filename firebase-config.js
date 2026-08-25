d5ca2fce5b405f8220d749b413a5dfc393d5cb2a
kongchang <ytctest2026@gmail.com>
2026-08-25 09:46:33 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCL6EkpCE9l_ahxKwvreTYbJBJ0GauXa4c",
+  authDomain: "data-track-23254.firebaseapp.com",
+  projectId: "data-track-23254",
+  storageBucket: "data-track-23254.firebasestorage.app",
+  messagingSenderId: "909992107147",
+  appId: "1:909992107147:web:6fc88511c14519c6a1888e",
+  measurementId: "G-DM0EKBQWE7"
+};
+
